486a3f371d4f391358de796085d0edabd47fb570
unknown <haoxiang.sun@sophgo-inc.com>
2023-07-31 01:08:04 +0800

    SN version 1.0

--- verilog/SN/fifo.sv
+++ verilog/SN/fifo.sv
@@ -1,4 +1,4 @@
-module fifo #(
+module fifo#(
     parameter   DEPTH   =   16  ,
     parameter   WIDTH   =   64  
 )(
@@ -12,6 +12,7 @@ module fifo #(
     input                   flush   ,//当tracker里面aw的length和收到w的数量对等的时候，进行flush操作，
     output                  flush_done,
     output  [10:0]          tag     ,//就是该fifo存的wid
+    input                   stall   ,
     //下游
     input                   ready_in,
     output                  valid_out,
@@ -19,26 +20,69 @@ module fifo #(
 );
     localparam  LOG2_DEPTH = $clog2(DEPTH)  ;
 
-    wire
+    wire		full	;
+	reg [10:0]	tag_q	;
+	reg			flushing;	
+	wire		empty	;
+	wire		pop		;
+	reg			valid_q	;
+	wire		active	;
 
-synchronous_fifo u_sync_fifo # (
-    .DWIDTH(WIDTH), 
-    .AWIDTH(LOG2_DEPTH), 
-    .DEPTH(DEPTH)
-    )(
-    .clk(clk),
-    .reset(rst),
-    .push(),
-    .in(fifo_in),
-    .pop(),
-    .out(fifo_out),
-    .empty(),
-    .almostempty(),
-    .full(),
-    .almostfull(),
-    .num(fifo_cnt)
+
+assign	ready_out	=	(!full)	&	(!flushing)	;	//FIFO is locked when flushing
+assign	valid_out	=	pop	;	
+assign	pop			=	ready_in	&	flushing	&	(!empty)	&	(!stall);
+assign	flush_done	=	flushing	&	empty	;
+assign	tag			=	tag_q	;
+assign	active		=	ready_in	&	valid_q	;
+
+always@(posedge clk)
+begin
+	if(rst)
+		valid_q		<=	1'b0	;
+	else if(pop)
+		valid_q		<=	1'b1	;
+	else
+		valid_q		<=	1'b0	;
+end
+
+always@(posedge clk)
+begin
+	if(rst)
+		tag_q[10:0]	<=	11'b0	;
+	else if(valid_in)
+		tag_q[10:0]	<=	wid[10:0]	;
+	else
+		tag_q[10:0]	<=	tag_q[10:0]	;
+end
+
+always@(posedge clk)
+begin
+	if(rst)
+		flushing	<=	1'b0	;
+	else if(flush)
+		flushing	<=	1'b1	;
+	else if(flush_done)
+		flushing	<=	1'b0	;
+	else
+		flushing	<=	flushing;
+end
+
+sync_fifo #(
+  .DEPTH(DEPTH),
+  .WIDTH(WIDTH)
+)
+u_sync_fifo
+(
+  .clk(clk),
+  .rst_n(!rst),
+  .push(valid_in),
+  .data_in(fifo_in),
+  .pop(pop),
+  .data_out(fifo_out),
+  .empty(empty),
+  .full(full),
+  .flush(0)
 );
-
-
 
 endmodule
